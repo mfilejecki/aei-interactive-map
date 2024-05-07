455a16b4d9003637fccf5e8b7123dd57affb01b2
filus4 <filej1025@gmail.com>
2024-05-07 17:12:54 +0200

    next project initiation

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "AEI Interactive Map",
+  description:
+    "Silesian University of Technology: Faculty of Automatic Control, Electronics and Computer Science interactive map",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
