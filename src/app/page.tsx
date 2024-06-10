2db85c4f192463d8c31c147044539113c537c7d8
filus4 <filej1025@gmail.com>
2024-06-10 19:35:20 +0200

    working project with loaded custom geojson

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,23 @@
+"use client";
+import React from "react";
+import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import geojsonData from "../maps/floor_1.geo.json";
+
 export default function Home() {
-  return <main>Home page</main>;
+  return (
+    <main>
+      <MapContainer
+        center={[50.2945, 18.6714]}
+        zoom={13}
+        style={{ height: "100vh", width: "100%" }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <GeoJSON data={geojsonData} />
+      </MapContainer>
+    </main>
+  );
 }
